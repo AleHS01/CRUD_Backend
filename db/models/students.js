2d14fa9fe975a88a36d0f6af2eed29fd4a30b8b0
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2023-06-28 15:49:14 -0400

    student model finish

--- db/models/students.js
+++ db/models/students.js
@@ -1,1 +1,30 @@
 const { DataTypes } = require("sequelize");
+const db = require("../db");
+
+const Students = db.define("shoes", {
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: DataTypes.STRING,
+    defaultValue: "",
+  },
+  gpa: {
+    type: DataTypes.DECIMAL,
+    validate: {
+      min: 0.0,
+      max: 4.0,
+    },
+  },
+});
+
+module.exports = Students;
