3ab7e216c59f7658cda7c6bac80b542d4aca7cca
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2023-06-28 15:35:52 -0400

    adding db and index file to db folder

---
+++ db/db.js
@@ -0,0 +1,8 @@
+const { Sequelize } = require("sequelize");
+const { name } = require("../package.json");
+
+const db = new Sequelize(`postgres://localhost:5432/${name}`, {
+  logging: false,
+});
+
+module.exports = db;
