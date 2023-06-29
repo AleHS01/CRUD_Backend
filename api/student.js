f7e7fe46898eafdccdf58ab434c9594d0ed00d05
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2023-06-29 14:45:35 -0400

    student test route

---
+++ api/student.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const { Student } = require("../database/models");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const allStudents = await Student.findAll();
+
+    allStudents
+      ? res.status(200).json(allStudents)
+      : res.status(404).send("Students List Not Found");
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
