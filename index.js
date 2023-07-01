8e9ba451acfc3c91978ac8f5c3297d60f0484c28
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2023-06-30 22:36:57 -0400

    student can be access with the id and also get the campus associated with

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const db = require("./database/db.js");
 const { Student, Campus } = require("./database/models/index.js");
 const PORT = 8080;
 const cors = require("cors");
-app.use(cors);
+app.use(cors());
 
 // Mount on API
 app.use("/api", require("./api"));
@@ -20,7 +20,7 @@ const serverRun = () => {
 async function main() {
   //await syncDB();
   console.log("This is going to print models: ", db.models);
-  await db.drop();
+  //await db.drop();
   //await db.sync({ force: true });
   await db.sync();
   //await seed();
